cc5ffda470e2a51601fd9f376a0ece00d462a3f0
Michael Cuneo <michaelcuneo75@gmail.com>
2026-07-22 23:04:35 +1000

    Add deterministic biome settings

--- Source/Orakai/CubusCore/Data/CubusGeologyProfile.h
+++ Source/Orakai/CubusCore/Data/CubusGeologyProfile.h
@@ -13,20 +13,10 @@ struct FCubusStrataLayer
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Cubus|Geology")
     int32 MaterialId = 1;
 
-    UPROPERTY(
-        EditAnywhere,
-        BlueprintReadWrite,
-        Category = "Cubus|Geology",
-        meta = (ClampMin = "0", UIMin = "0", UIMax = "512")
-    )
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Cubus|Geology", meta = (ClampMin = "0", UIMin = "0", UIMax = "512"))
     int32 MinimumDepth = 0;
 
-    UPROPERTY(
-        EditAnywhere,
-        BlueprintReadWrite,
-        Category = "Cubus|Geology",
-        meta = (ClampMin = "1", UIMin = "1", UIMax = "512")
-    )
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Cubus|Geology", meta = (ClampMin = "1", UIMin = "1", UIMax = "512"))
     int32 MaximumDepth = 32;
 };
 
@@ -47,31 +37,17 @@ struct FCubusOreRule
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Cubus|Geology")
     int32 MaximumWorldZ = 32;
 
-    UPROPERTY(
-        EditAnywhere,
-        BlueprintReadWrite,
-        Category = "Cubus|Geology",
-        meta = (ClampMin = "0.000001", UIMin = "0.001", UIMax = "0.5")
-    )
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Cubus|Geology", meta = (ClampMin = "0.000001", UIMin = "0.001", UIMax = "0.5"))
     float Frequency = 0.04f;
 
-    UPROPERTY(
-        EditAnywhere,
-        BlueprintReadWrite,
-        Category = "Cubus|Geology",
-        meta = (ClampMin = "-1.0", ClampMax = "1.0", UIMin = "-1.0", UIMax = "1.0")
-    )
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Cubus|Geology", meta = (ClampMin = "-1.0", ClampMax = "1.0", UIMin = "-1.0", UIMax = "1.0"))
     float Threshold = 0.75f;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Cubus|Geology")
     TArray<int32> ReplaceableMaterialIds;
 };
 
-UCLASS(
-    BlueprintType,
-    ClassGroup = "Cubus",
-    meta = (DisplayName = "Cubus Geology Profile")
-)
+UCLASS(BlueprintType, ClassGroup = "Cubus", meta = (DisplayName = "Cubus Geology Profile"))
 class ORAKAI_API UCubusGeologyProfile : public UDataAsset
 {
     GENERATED_BODY()
@@ -83,68 +59,28 @@ public:
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cubus|Geology|Rivers")
     bool bGenerateRivers = false;
 
-    UPROPERTY(
-        EditAnywhere,
-        BlueprintReadOnly,
-        Category = "Cubus|Geology|Rivers",
-        meta = (ClampMin = "0.000001", UIMin = "0.0001", UIMax = "0.02")
-    )
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cubus|Geology|Rivers", meta = (ClampMin = "0.000001", UIMin = "0.0001", UIMax = "0.02"))
     float RiverFrequency = 0.0025f;
 
-    UPROPERTY(
-        EditAnywhere,
-        BlueprintReadOnly,
-        Category = "Cubus|Geology|Rivers",
-        meta = (ClampMin = "0.0", ClampMax = "1.0", UIMin = "0.0", UIMax = "0.5")
-    )
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cubus|Geology|Rivers", meta = (ClampMin = "0.0", ClampMax = "1.0", UIMin = "0.0", UIMax = "0.5"))
     float RiverChannelWidth = 0.055f;
 
-    UPROPERTY(
-        EditAnywhere,
-        BlueprintReadOnly,
-        Category = "Cubus|Geology|Rivers",
-        meta = (ClampMin = "0.001", ClampMax = "1.0", UIMin = "0.01", UIMax = "1.0")
-    )
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cubus|Geology|Rivers", meta = (ClampMin = "0.001", ClampMax = "1.0", UIMin = "0.01", UIMax = "1.0"))
     float RiverValleyWidth = 0.22f;
 
-    UPROPERTY(
-        EditAnywhere,
-        BlueprintReadOnly,
-        Category = "Cubus|Geology|Rivers",
-        meta = (ClampMin = "0.0", UIMin = "0.0", UIMax = "32.0")
-    )
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cubus|Geology|Rivers", meta = (ClampMin = "0.0", UIMin = "0.0", UIMax = "32.0"))
     float RiverValleyDepth = 7.0f;
 
-    UPROPERTY(
-        EditAnywhere,
-        BlueprintReadOnly,
-        Category = "Cubus|Geology|Rivers",
-        meta = (ClampMin = "1", UIMin = "1", UIMax = "24")
-    )
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cubus|Geology|Rivers", meta = (ClampMin = "1", UIMin = "1", UIMax = "24"))
     int32 RiverChannelDepth = 4;
 
-    UPROPERTY(
-        EditAnywhere,
-        BlueprintReadOnly,
-        Category = "Cubus|Geology|Rivers",
-        meta = (ClampMin = "1", UIMin = "1", UIMax = "8")
-    )
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cubus|Geology|Rivers", meta = (ClampMin = "1", UIMin = "1", UIMax = "8"))
     int32 RiverWaterDepth = 2;
 
-    UPROPERTY(
-        EditAnywhere,
-        BlueprintReadOnly,
-        Category = "Cubus|Geology|Rivers",
-        meta = (ClampMin = "0.0", UIMin = "0.0", UIMax = "256.0")
-    )
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cubus|Geology|Rivers", meta = (ClampMin = "0.0", UIMin = "0.0", UIMax = "256.0"))
     float RiverWarpAmplitude = 48.0f;
 
-    UPROPERTY(
-        EditAnywhere,
-        BlueprintReadOnly,
-        Category = "Cubus|Geology|Rivers",
-        meta = (ClampMin = "0.000001", UIMin = "0.0001", UIMax = "0.05")
-    )
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cubus|Geology|Rivers", meta = (ClampMin = "0.000001", UIMin = "0.0001", UIMax = "0.05"))
     float RiverWarpFrequency = 0.006f;
 
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cubus|Geology|Rivers")
@@ -152,6 +88,36 @@ public:
 
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cubus|Geology|Rivers")
     int32 RiverWaterMaterialId = 5;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cubus|Geology|Biomes")
+    bool bGenerateBiomes = false;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cubus|Geology|Biomes", meta = (ClampMin = "0.000001", UIMin = "0.0001", UIMax = "0.05"))
+    float BiomeFrequency = 0.004f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cubus|Geology|Biomes", meta = (ClampMin = "0.0", ClampMax = "1.0", UIMin = "0.0", UIMax = "1.0"))
+    float ForestThreshold = 0.15f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cubus|Geology|Biomes", meta = (ClampMin = "0.0", UIMin = "0.0", UIMax = "16.0"))
+    float WetlandRiverDistance = 0.10f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cubus|Geology|Biomes", meta = (ClampMin = "0.0", UIMin = "0.0", UIMax = "32.0"))
+    float RockySlopeThreshold = 4.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cubus|Geology|Biomes")
+    int32 RockyMinimumWorldZ = 48;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cubus|Geology|Biomes")
+    int32 PlainsSurfaceMaterialId = 1;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cubus|Geology|Biomes")
+    int32 ForestSurfaceMaterialId = 7;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cubus|Geology|Biomes")
+    int32 RockySurfaceMaterialId = 3;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cubus|Geology|Biomes")
+    int32 WetlandSurfaceMaterialId = 8;
 
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cubus|Geology|Caves")
     bool bGenerateCaves = false;
@@ -162,36 +128,16 @@ public:
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cubus|Geology|Caves")
     int32 CaveMaximumWorldZ = 24;
 
-    UPROPERTY(
-        EditAnywhere,
-        BlueprintReadOnly,
-        Category = "Cubus|Geology|Caves",
-        meta = (ClampMin = "1", UIMin = "1", UIMax = "32")
-    )
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cubus|Geology|Caves", meta = (ClampMin = "1", UIMin = "1", UIMax = "32"))
     int32 CaveSurfaceClearance = 5;
 
-    UPROPERTY(
-        EditAnywhere,
-        BlueprintReadOnly,
-        Category = "Cubus|Geology|Caves",
-        meta = (ClampMin = "0.000001", UIMin = "0.005", UIMax = "0.2")
-    )
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cubus|Geology|Caves", meta = (ClampMin = "0.000001", UIMin = "0.005", UIMax = "0.2"))
     float CavePrimaryFrequency = 0.035f;
 
-    UPROPERTY(
-        EditAnywhere,
-        BlueprintReadOnly,
-        Category = "Cubus|Geology|Caves",
-        meta = (ClampMin = "0.000001", UIMin = "0.005", UIMax = "0.2")
-    )
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cubus|Geology|Caves", meta = (ClampMin = "0.000001", UIMin = "0.005", UIMax = "0.2"))
     float CaveSecondaryFrequency = 0.07f;
 
-    UPROPERTY(
-        EditAnywhere,
-        BlueprintReadOnly,
-        Category = "Cubus|Geology|Caves",
-        meta = (ClampMin = "0.0", ClampMax = "1.0", UIMin = "0.0", UIMax = "1.0")
-    )
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cubus|Geology|Caves", meta = (ClampMin = "0.0", ClampMax = "1.0", UIMin = "0.0", UIMax = "1.0"))
     float CaveThreshold = 0.16f;
 
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cubus|Geology|Ore")
