d1cfe075c44e3578577ebeb61d6be2d222a53baf
Michael Cuneo <michaelcuneo75@gmail.com>
2026-07-24 05:08:43 +1000

    Force collision generation on streamed voxel chunks

--- Source/Orakai/CubusCore/Actors/CubusVoxelChunkMobility.cpp
+++ Source/Orakai/CubusCore/Actors/CubusVoxelChunkMobility.cpp
@@ -19,6 +19,8 @@ namespace CubusVoxelChunkMobility
             return;
         }
 
+        ChunkActor->SetGenerateCollision(true);
+
         USceneComponent* RootComponent =
             ChunkActor->GetRootComponent();
 
