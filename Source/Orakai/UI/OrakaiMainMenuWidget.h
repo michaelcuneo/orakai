40c0e2e83f736966fca61c7d837545181489a4b4
Michael Cuneo <michaelcuneo75@gmail.com>
2026-07-26 11:32:56 +1000

    Build native main menu before Slate reconstruction

--- Source/Orakai/UI/OrakaiMainMenuWidget.h
+++ Source/Orakai/UI/OrakaiMainMenuWidget.h
@@ -4,6 +4,7 @@
 #include "Blueprint/UserWidget.h"
 #include "OrakaiMainMenuWidget.generated.h"
 
+class SWidget;
 class UButton;
 
 UCLASS()
@@ -12,9 +13,12 @@ class ORAKAI_API UOrakaiMainMenuWidget : public UUserWidget
     GENERATED_BODY()
 
 protected:
+    virtual TSharedRef<SWidget> RebuildWidget() override;
     virtual void NativeConstruct() override;
 
 private:
+    void BuildWidgetTree();
+
     UFUNCTION()
     void HandleStartGame();
 
