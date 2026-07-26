fc5496050923a995ac350c34edf7155e0c9a0a8b
Michael Cuneo <michaelcuneo75@gmail.com>
2026-07-26 11:39:01 +1000

    Remove gameplay menu input lock

--- Source/Orakai/OrakaiGameMode.cpp
+++ Source/Orakai/OrakaiGameMode.cpp
@@ -2,45 +2,7 @@
 
 #include "OrakaiGameMode.h"
 
-#include "Blueprint/UserWidget.h"
-#include "GameFramework/PlayerController.h"
-#include "Kismet/GameplayStatics.h"
-#include "UI/OrakaiMainMenuWidget.h"
-
 AOrakaiGameMode::AOrakaiGameMode()
 {
-}
-
-void AOrakaiGameMode::BeginPlay()
-{
-	Super::BeginPlay();
-
-	APlayerController* PlayerController =
-		UGameplayStatics::GetPlayerController(this, 0);
-
-	if (!IsValid(PlayerController) || !PlayerController->IsLocalController())
-	{
-		return;
-	}
-
-	MainMenuWidget = CreateWidget<UOrakaiMainMenuWidget>(
-		PlayerController,
-		UOrakaiMainMenuWidget::StaticClass()
-	);
-
-	if (!IsValid(MainMenuWidget))
-	{
-		return;
-	}
-
-	MainMenuWidget->AddToPlayerScreen(100);
-
-	PlayerController->SetIgnoreMoveInput(true);
-	PlayerController->SetIgnoreLookInput(true);
-	PlayerController->SetShowMouseCursor(true);
-
-	FInputModeUIOnly InputMode;
-	InputMode.SetWidgetToFocus(MainMenuWidget->TakeWidget());
-	InputMode.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
-	PlayerController->SetInputMode(InputMode);
+	// stub
 }
