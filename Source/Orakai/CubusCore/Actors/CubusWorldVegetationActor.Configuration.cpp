8334fd5c5428ba240a11de43727e9606505efe88
Michael Cuneo <michaelcuneo75@gmail.com>
2026-07-27 14:15:43 +1000

    Remove obsolete world vegetation PCG configuration unit

--- Source/Orakai/CubusCore/Actors/CubusWorldVegetationActor.Configuration.cpp
+++
@@ -1,4 +0,0 @@
-#include "CubusCore/Actors/CubusWorldVegetationActor.h"
-
-// ConfigureForWorld is implemented in CubusWorldVegetationActor.cpp.
-// Keep this translation unit so existing project file organisation remains stable.
