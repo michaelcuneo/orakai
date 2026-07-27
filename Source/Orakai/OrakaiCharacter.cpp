23f5291ccbdc4ce70647dc4381d70a4b77b0fd76
Michael Cuneo <michaelcuneo75@gmail.com>
2026-07-27 12:01:14 +1000

    Fixed Nanites

--- Source/Orakai/OrakaiCharacter.cpp
+++ Source/Orakai/OrakaiCharacter.cpp
@@ -36,7 +36,7 @@ AOrakaiCharacter::AOrakaiCharacter()
 	GetCharacterMovement()->BrakingDecelerationFalling = 1500.0f;
 
 	// Create a camera boom (pulls in towards the player if there is a collision)
-	CameraBoom = CreateDefaultSubobject<USpringArmComponent
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
 	CameraBoom->TargetArmLength = 400.0f;
 	CameraBoom->bUsePawnControlRotation = true;
