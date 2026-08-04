af9e7592fe9eaf657c6b77888f5bff759a1a3a18
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-04 10:50:41 +1000

    Render neighbour-classified geological ramps

--- Source/Orakai/CubusCore/Meshing/CubusBlockMesher.cpp
+++ Source/Orakai/CubusCore/Meshing/CubusBlockMesher.cpp
@@ -40,6 +40,76 @@ namespace CubusBlockMesher
 
         return 0.0f;
     }
+
+    bool ShouldRenderSolidFace(
+        const FCubusBlockVoxel* NeighbourVoxel,
+        const UCubusMaterialRegistry* MaterialRegistry
+    )
+    {
+        if (NeighbourVoxel == nullptr || NeighbourVoxel->IsEmpty())
+        {
+            return true;
+        }
+
+        if (NeighbourVoxel->IsWater())
+        {
+            return true;
+        }
+
+        const bool bNeighbourOccludesFace =
+            MaterialRegistry != nullptr
+                ? MaterialRegistry->OccludesBlockFaces(NeighbourVoxel->MaterialId)
+                : true;
+
+        return !bNeighbourOccludesFace;
+    }
+
+    void ResolveRampAxes(
+        const ECubusBlockSurfaceShape Shape,
+        FVector& OutUphillDirection,
+        FVector& OutSideDirection,
+        FIntVector& OutNegativeSideOffset,
+        FIntVector& OutPositiveSideOffset
+    )
+    {
+        switch (Shape)
+        {
+            case ECubusBlockSurfaceShape::RampPositiveX:
+                OutUphillDirection = FVector(-1.0f, 0.0f, 0.0f);
+                OutSideDirection = FVector(0.0f, 1.0f, 0.0f);
+                OutNegativeSideOffset = FIntVector(0, -1, 0);
+                OutPositiveSideOffset = FIntVector(0, 1, 0);
+                break;
+
+            case ECubusBlockSurfaceShape::RampNegativeX:
+                OutUphillDirection = FVector(1.0f, 0.0f, 0.0f);
+                OutSideDirection = FVector(0.0f, 1.0f, 0.0f);
+                OutNegativeSideOffset = FIntVector(0, -1, 0);
+                OutPositiveSideOffset = FIntVector(0, 1, 0);
+                break;
+
+            case ECubusBlockSurfaceShape::RampPositiveY:
+                OutUphillDirection = FVector(0.0f, -1.0f, 0.0f);
+                OutSideDirection = FVector(1.0f, 0.0f, 0.0f);
+                OutNegativeSideOffset = FIntVector(-1, 0, 0);
+                OutPositiveSideOffset = FIntVector(1, 0, 0);
+                break;
+
+            case ECubusBlockSurfaceShape::RampNegativeY:
+                OutUphillDirection = FVector(0.0f, 1.0f, 0.0f);
+                OutSideDirection = FVector(1.0f, 0.0f, 0.0f);
+                OutNegativeSideOffset = FIntVector(-1, 0, 0);
+                OutPositiveSideOffset = FIntVector(1, 0, 0);
+                break;
+
+            default:
+                OutUphillDirection = FVector::ZeroVector;
+                OutSideDirection = FVector::ZeroVector;
+                OutNegativeSideOffset = FIntVector::ZeroValue;
+                OutPositiveSideOffset = FIntVector::ZeroValue;
+                break;
+        }
+    }
 }
 
 void FCubusBlockMesher::BuildChunk(
@@ -121,6 +191,34 @@ void FCubusBlockMesher::BuildChunk(
                         (static_cast<float>(Z) + 0.5f) * VoxelSize
                     );
 
+                if (!bCurrentIsWater)
+                {
+                    const FCubusBlockSurfaceClassification Classification =
+                        FCubusBlockSurfaceClassifier::Classify(
+                            Neighborhood,
+                            MaterialRegistry,
+                            X,
+                            Y,
+                            Z
+                        );
+
+                    if (Classification.IsRamp())
+                    {
+                        OutGeneratedFaceCount += AddRamp(
+                            MaterialMesh,
+                            Neighborhood,
+                            MaterialRegistry,
+                            VoxelCentre,
+                            HalfVoxelSize,
+                            X,
+                            Y,
+                            Z,
+                            Classification.Shape
+                        );
+                        continue;
+                    }
+                }
+
                 for (int32 FaceIndex = 0; FaceIndex < CubusBlockMesher::FaceCount; ++FaceIndex)
                 {
                     const FIntVector NeighbourPosition =
@@ -175,6 +273,140 @@ void FCubusBlockMesher::BuildChunk(
             }
         }
     }
+}
+
+int32 FCubusBlockMesher::AddRamp(
+    FCubusMeshData& MeshData,
+    const FCubusBlockChunkNeighborhood& Neighborhood,
+    const UCubusMaterialRegistry* MaterialRegistry,
+    const FVector& VoxelCentre,
+    const float HalfVoxelSize,
+    const int32 X,
+    const int32 Y,
+    const int32 Z,
+    const ECubusBlockSurfaceShape Shape
+)
+{
+    FVector UphillDirection;
+    FVector SideDirection;
+    FIntVector NegativeSideOffset;
+    FIntVector PositiveSideOffset;
+
+    CubusBlockMesher::ResolveRampAxes(
+        Shape,
+        UphillDirection,
+        SideDirection,
+        NegativeSideOffset,
+        PositiveSideOffset
+    );
+
+    if (UphillDirection.IsNearlyZero() || SideDirection.IsNearlyZero())
+    {
+        return 0;
+    }
+
+    const FVector UpDirection = FVector::UpVector;
+
+    const FVector HighNegativeSide =
+        VoxelCentre +
+        UphillDirection * HalfVoxelSize -
+        SideDirection * HalfVoxelSize +
+        UpDirection * HalfVoxelSize;
+
+    const FVector HighPositiveSide =
+        VoxelCentre +
+        UphillDirection * HalfVoxelSize +
+        SideDirection * HalfVoxelSize +
+        UpDirection * HalfVoxelSize;
+
+    const FVector LowNegativeSide =
+        VoxelCentre -
+        UphillDirection * HalfVoxelSize -
+        SideDirection * HalfVoxelSize -
+        UpDirection * HalfVoxelSize;
+
+    const FVector LowPositiveSide =
+        VoxelCentre -
+        UphillDirection * HalfVoxelSize +
+        SideDirection * HalfVoxelSize -
+        UpDirection * HalfVoxelSize;
+
+    const FVector BottomHighNegativeSide =
+        VoxelCentre +
+        UphillDirection * HalfVoxelSize -
+        SideDirection * HalfVoxelSize -
+        UpDirection * HalfVoxelSize;
+
+    const FVector BottomHighPositiveSide =
+        VoxelCentre +
+        UphillDirection * HalfVoxelSize +
+        SideDirection * HalfVoxelSize -
+        UpDirection * HalfVoxelSize;
+
+    const FVector SlopeNormal =
+        (-UphillDirection + UpDirection).GetSafeNormal();
+
+    AddFace(
+        MeshData,
+        HighNegativeSide,
+        LowNegativeSide,
+        LowPositiveSide,
+        HighPositiveSide,
+        SlopeNormal,
+        CubusBlockMesher::GetMaterialSelector(CubusBlockMesher::PositiveZ)
+    );
+
+    int32 GeneratedFaceCount = 1;
+
+    const FCubusBlockVoxel* NegativeSideNeighbour = Neighborhood.GetVoxel(
+        X + NegativeSideOffset.X,
+        Y + NegativeSideOffset.Y,
+        Z + NegativeSideOffset.Z
+    );
+
+    if (
+        CubusBlockMesher::ShouldRenderSolidFace(
+            NegativeSideNeighbour,
+            MaterialRegistry
+        )
+    )
+    {
+        AddTriangle(
+            MeshData,
+            HighNegativeSide,
+            BottomHighNegativeSide,
+            LowNegativeSide,
+            -SideDirection,
+            CubusBlockMesher::GetMaterialSelector(CubusBlockMesher::PositiveX)
+        );
+        ++GeneratedFaceCount;
+    }
+
+    const FCubusBlockVoxel* PositiveSideNeighbour = Neighborhood.GetVoxel(
+        X + PositiveSideOffset.X,
+        Y + PositiveSideOffset.Y,
+        Z + PositiveSideOffset.Z
+    );
+
+    if (
+        CubusBlockMesher::ShouldRenderSolidFace(
+            PositiveSideNeighbour,
+            MaterialRegistry
+        )
+    )
+    {
+        AddTriangle(
+            MeshData,
+            HighPositiveSide,
+            LowPositiveSide,
+            BottomHighPositiveSide,
+            SideDirection,
+            CubusBlockMesher::GetMaterialSelector(CubusBlockMesher::PositiveX)
+        );
+        ++GeneratedFaceCount;
+    }
+
+    return GeneratedFaceCount;
 }
 
 void FCubusBlockMesher::AddVoxelFace(
@@ -302,6 +534,49 @@ void FCubusBlockMesher::AddFace(
     const FProcMeshTangent Tangent(TangentDirection, false);
 
     MeshData.Tangents.Add(Tangent);
+    MeshData.Tangents.Add(Tangent);
+    MeshData.Tangents.Add(Tangent);
+    MeshData.Tangents.Add(Tangent);
+}
+
+void FCubusBlockMesher::AddTriangle(
+    FCubusMeshData& MeshData,
+    const FVector& Vertex0,
+    const FVector& Vertex1,
+    const FVector& Vertex2,
+    const FVector& Normal,
+    const float MaterialSelector
+)
+{
+    const int32 FirstVertexIndex = MeshData.Vertices.Num();
+
+    MeshData.Vertices.Add(Vertex0);
+    MeshData.Vertices.Add(Vertex1);
+    MeshData.Vertices.Add(Vertex2);
+
+    MeshData.Triangles.Append(
+    {
+        FirstVertexIndex + 0,
+        FirstVertexIndex + 1,
+        FirstVertexIndex + 2
+    });
+
+    MeshData.Normals.Add(Normal);
+    MeshData.Normals.Add(Normal);
+    MeshData.Normals.Add(Normal);
+
+    MeshData.UV0.Add(FVector2D(0.0f, 1.0f));
+    MeshData.UV0.Add(FVector2D(0.0f, 0.0f));
+    MeshData.UV0.Add(FVector2D(1.0f, 0.0f));
+
+    const FLinearColor FaceColor(1.0f, 1.0f, 1.0f, MaterialSelector);
+    MeshData.VertexColors.Add(FaceColor);
+    MeshData.VertexColors.Add(FaceColor);
+    MeshData.VertexColors.Add(FaceColor);
+
+    const FVector TangentDirection = (Vertex1 - Vertex0).GetSafeNormal();
+    const FProcMeshTangent Tangent(TangentDirection, false);
+
     MeshData.Tangents.Add(Tangent);
     MeshData.Tangents.Add(Tangent);
     MeshData.Tangents.Add(Tangent);
