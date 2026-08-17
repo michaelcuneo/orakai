77dc3825995ee2fef372753068e377d825d9877b
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-17 11:38:42 +1000

    Allow alluvial deposition to modify DEM tiles

--- Source/Orakai/CubusCore/Generation/CubusTerrainRaster.h
+++ Source/Orakai/CubusCore/Generation/CubusTerrainRaster.h
@@ -62,6 +62,7 @@ private:
     friend class FCubusTerrainRasterBuilder;
     friend class FCubusTerrainCarving;
     friend class FCubusTerrainErosion;
+    friend class FCubusTerrainDeposition;
 
     static float CubicInterpolate(float P0, float P1, float P2, float P3, float Alpha);
     int32 StorageIndex(int32 GridX, int32 GridY) const;
