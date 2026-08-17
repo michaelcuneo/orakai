ccfeacf8689bef8c0874ae2a6125e67ae16fc563
Michael Cuneo <michaelcuneo75@gmail.com>
2026-08-17 13:23:53 +1000

    Add generated terrain spawn picker to loading UI

--- Source/Orakai/CubusCore/UI/OrakaiWorldLoadingWidget.h
+++ Source/Orakai/CubusCore/UI/OrakaiWorldLoadingWidget.h
@@ -4,8 +4,13 @@
 #include "Blueprint/UserWidget.h"
 #include "OrakaiWorldLoadingWidget.generated.h"
 
+class UBorder;
+class UButton;
+class UCanvasPanel;
+class UImage;
 class UProgressBar;
 class UTextBlock;
+class UTexture2D;
 
 UCLASS()
 class ORAKAI_API UOrakaiWorldLoadingWidget : public UUserWidget
@@ -14,14 +19,54 @@ class ORAKAI_API UOrakaiWorldLoadingWidget : public UUserWidget
 
 public:
 	void SetLoadingState(float Progress, const FText& Status);
+	void SetSpawnSelectionAvailable(bool bAvailable);
+	void SetSpawnPromotionActive(bool bActive);
+	bool IsSpawnSelectionAvailable() const { return bSpawnSelectionAvailable; }
 
 protected:
 	virtual void NativeConstruct() override;
+	virtual FReply NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
 
 private:
+	UFUNCTION()
+	void HandleSpawnClicked();
+
+	void BuildGeneratedPreviewTexture();
+	void UpdateSelectionMarker(const FVector2D& PreviewUV);
+	void RefreshSpawnUi();
+
 	UPROPERTY(Transient)
 	TObjectPtr<UProgressBar> ProgressBar = nullptr;
 
 	UPROPERTY(Transient)
 	TObjectPtr<UTextBlock> StatusText = nullptr;
+
+	UPROPERTY(Transient)
+	TObjectPtr<UTextBlock> SpawnInstructionText = nullptr;
+
+	UPROPERTY(Transient)
+	TObjectPtr<UTextBlock> SpawnCoordinateText = nullptr;
+
+	UPROPERTY(Transient)
+	TObjectPtr<UCanvasPanel> PreviewCanvas = nullptr;
+
+	UPROPERTY(Transient)
+	TObjectPtr<UImage> PreviewImage = nullptr;
+
+	UPROPERTY(Transient)
+	TObjectPtr<UBorder> SpawnMarker = nullptr;
+
+	UPROPERTY(Transient)
+	TObjectPtr<UButton> SpawnButton = nullptr;
+
+	UPROPERTY(Transient)
+	TObjectPtr<UTextBlock> SpawnButtonText = nullptr;
+
+	UPROPERTY(Transient)
+	TObjectPtr<UTexture2D> RuntimePreviewTexture = nullptr;
+
+	FVector2D SelectedPreviewUV = FVector2D(0.5, 0.5);
+	bool bHasSpawnSelection = false;
+	bool bSpawnSelectionAvailable = false;
+	bool bSpawnPromotionActive = false;
 };
